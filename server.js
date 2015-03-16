3e41d9ad7771eab45755a91fb4f12eea9110dfa9
tsatse <joachim.revol-tissot.ext@francetv.fr>
2015-03-16 13:36:04 +0100

    adding local linked packages support

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var fs = require("fs")
+var path = require("path")
 var restify = require("restify")
+var program = require("commander")
+var npm = require("npm")
 var Package = require("./lib/package")
-var program = require('commander')
 
-var packageWithoutVersionPattern = /^\/([^@]+)[\/]?$/ // {name}
+var packageWithoutVersionPattern = /^\/([^@\/]+)[\/]?(.*)$/ // {name}/{filepath}
 var packageFilePattern = /^\/(.*)@(\d+\.\d+\.\d+)[\/]?(.*)$/ // {name}@{version}/{filepath}
 program
   .version('2.0.0')
@@ -19,7 +21,7 @@ function serveFile(req, res, next) {
   var name =    req.params[0]
   var version = req.params[1]
   var file =    req.params[2]
-  var pkg = new Package(name, version)
+  var pkg = new Package(name, version, {registry: registry})
 
   // Show generated index if filename is absent
   if (!file) {
@@ -41,22 +43,48 @@ function serveFile(req, res, next) {
 }
 
 function redirectToVersionedPackage(req, res, next) {
-  console.log("redirectToVersionedPackage", req.params[0])
-  require("superagent").get(registry + req.params[0], function(rez){
-    if (!rez.ok) {
-      return res.send(404, {error: "package not found: " + req.params[0]})
-    }
+  console.log("redirectToVersionedPackage", req.params[0], "with path", req.params[1])
+  npm.load({}, function() {
+    var npmPrefix = npm.config.get("prefix")
+    var linkedModulePath = path.join(npmPrefix, "lib/node_modules",  req.params[0])
 
-    var pkg = rez.body
-    res.header("Location", "/" + pkg.name + "@" + pkg["dist-tags"].latest)
-    return res.send(302)
-  })
+    console.log('checking whether ', linkedModulePath, 'exists')
+    if(fs.existsSync(linkedModulePath)) {
+      console.log('it exists')
+      var pkg = fs.readFileSync(path.join(linkedModulePath, 'package.json'))
+      console.log('just read ', path.join(linkedModulePath, 'package.json'))
+      pkg = JSON.parse(pkg)
+      console.log('and parsed it to ', pkg)
+      var redirPath = "/" + pkg.name + "@" + pkg.version;
+      if(req.params[1]) {
+        redirPath += "/" + req.params[1]
+      }
+      res.header("Location", redirPath)
+      console.log('now redirecting to ', redirPath)
+      return res.send(302)
+    }
+    else {
+      require("superagent").get(registry + req.params[0], function(rez){
+        if (!rez.ok) {
+          return res.send(404, {error: "package not found: " + req.params[0]})
+        }
+
+        var pkg = rez.body
+        var redirPath = "/" + pkg.name + "@" + pkg["dist-tags"].latest;
+        if(req.params[1]) {
+          redirPath += "/" + req.params[1]
+        }
+        res.header("Location", redirPath)
+        return res.send(302)
+      })
+    }
+  });
 }
 
 var server = module.exports = restify.createServer()
 server.use(restify.queryParser())
-server.get(packageWithoutVersionPattern, redirectToVersionedPackage)
 server.get(packageFilePattern, serveFile)
+server.get(packageWithoutVersionPattern, redirectToVersionedPackage)
 server.head(packageFilePattern, serveFile)
 server.listen(port, function() {
   console.log("%s listening at %s", server.name, server.url);
